0edf69831d2d6b72a7776c95b9d628459f0d05ea
Matt Fisher <beatmakerx@gmail.com>
2024-02-29 14:57:57 -0800

    Initial Commit

---
+++ github experiment/github_experimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_experimentApp.swift
+//  github experiment
+//
+//  Created by matt fisher on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct github_experimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
